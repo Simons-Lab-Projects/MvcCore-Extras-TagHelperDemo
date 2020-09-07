6fd95c30a661ff6d6f82c88efb8a9146993e6625
Simon Woodworth <s.woodworth@ucc.ie>
2020-09-07 17:45:23 +0100

    Add project files.

---
+++ TagHelperDemo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TagHelperDemo.Models;
+
+namespace TagHelperDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        [ValidateAntiForgeryToken]
+        public IActionResult IndexPost(Person p)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Index", p);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ TagHelperDemo/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TagHelperDemo.Models
+{
+    public class Person
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Address { get; set; }
+
+        [Required]
+        [Range(0, 120, ErrorMessage = "Please enter whole number between 0 and 120")]
+        public int Age { get; set; }
+    }
+   
+}
